d3e6ebc95159bd0576f2de809876df4e55b04c07
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-08-03 18:48:07 +0200

    Add recaman.py helper

---
+++ src/recaman.py
@@ -0,0 +1,22 @@
+"""Recamán's sequence generator"""
+from typing import Generator
+
+
+def recaman(steps: int) -> Generator[int, None, None]:
+    """Generate Recamán's sequence
+
+    :param steps: amount of iterations / length of iterator
+    :returns: None
+    """
+    previous = [0]
+
+    for step in range(1, steps):
+        last = previous[-1]
+        new = last - step
+
+        if new in previous or new < 0:
+            new = last + step
+
+        previous.append(new)
+
+        yield previous[-1]
